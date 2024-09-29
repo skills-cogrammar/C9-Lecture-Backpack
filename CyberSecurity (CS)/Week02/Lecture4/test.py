6a89cb00a053e709e8ad6973d1de650177aaca0d
Bonaventure Ogeto <bonaogeto1@gmail.com>
2024-09-29 14:02:26 +0300

    W02L2 repeat lecture

---
+++ CyberSecurity (CS)/Week02/Lecture4/test.py
@@ -0,0 +1,30 @@
+# votes = int(input("Enter the vote count: "))
+
+# if(votes > 100):
+#     print("The candidate is famous")
+# elif(votes > 50):
+#     print("The candidate is kind of famous")
+# elif(votes > 10):
+#     print("The candidate is not as popular")
+# else:
+#     print("The candidate should just retire")
+
+# age = int(input("Please enter your age: "))
+
+# if (age <=4):
+#     print("assign to group A")
+# elif(age>4 and age<8):
+#     print("assign to group B")
+# elif(age>=8 and age<=13):
+#     print("group C")
+# else:
+#     print("group D")
+
+x = 10
+
+if (x == 10):
+    print("Equal")
+elif (x > 5):
+    print("Greater")
+else:
+    print("Smaller")
