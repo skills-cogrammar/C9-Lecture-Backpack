70fb09334307ed2e87e10c4ee833f193c76ef9fd
ArmandleRouxx <armandlr@hyperiondev.com>
2024-10-03 12:42:04 +0200

    Changed folder name

--- CyberSecurity (CS)/Week03/TaskWalkthrough1/Task2/index.py
+++
@@ -1,5 +0,0 @@
-#Get what the user ate for lunch and display it including their name
-#Get lunch details
-lunch = input("What did you have for lunch?")
-#display the lunch details with their name
-print("Dan Walobwa had " + lunch + " for lunch.")
