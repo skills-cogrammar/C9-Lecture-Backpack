b68deac7b023593081bf7d6f9c09478f7354cdd3
zahra_moham <148523671+zahram-hypdev@users.noreply.github.com>
2024-11-06 09:54:28 +0200

    Added files created in CIW lecture

---
+++ CodingInterviewWorkshops (CIW)/Week02/singleLL.py
@@ -0,0 +1,19 @@
+import LinkedLists as ll
+
+sll = ll.SinglyLinkedList()
+
+sll.insert_at_beginning(1234)
+sll.traverse()
+print()
+
+sll.insert_at_beginning(4759)
+sll.traverse()
+print()
+
+sll.insert_at_end("cat")
+sll.traverse()
+print()
+
+sll.insert_at_beginning("dog")
+sll.traverse()
+print()
